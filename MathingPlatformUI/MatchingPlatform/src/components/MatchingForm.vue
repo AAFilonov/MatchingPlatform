056eb4251df15583b3e8127758bf241bfcf447cc
AFilonov <filon13p@gmail.com>
2023-03-18 12:39:33 +0300

    sumbit form added

--- MathingPlatformUI/MatchingPlatform/src/components/MatchingForm.vue
+++ MathingPlatformUI/MatchingPlatform/src/components/MatchingForm.vue
@@ -1,9 +1,79 @@
-<script lang="ts">
-import {ref, Component} from 'vue'
+<template>
+  <div>
+    <v-sheet class="mx-auto" min-width="400px">
+      <v-form @submit.prevent>
+        <v-combobox
+            v-model="problemSelected"
+            :items="problemData"
+            label="Problem type"
+            @select="problemOnChange"
+        ></v-combobox>
 
-export default {
+        <v-combobox
+            v-model="algSelected"
+            :items="algsList"
+            label="Alg type"
+        ></v-combobox>
+
+        <v-file-input
+            v-model="inputFile"
+            accept=".xlsx"
+            label="matching data file"
+            outlined
+        ></v-file-input>
+        
+        <v-text-field
+            v-model="matchingName"
+            :rules="this.rules"
+            label="Matching name"
+        ></v-text-field>
+        
+        <v-btn block class="mt-2" @click="onSubmit" type="submit">Submit</v-btn>
+      </v-form>
+    </v-sheet>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+
+interface Item {
+  title: string;
+  value: string;
+}
+
+interface Problem extends Item {
+  algs: Item[];
+}
+
+const problem_data: Problem[] = [
+  {
+    title: 'Stable marriage problem',
+    value: 'SMP',
+    algs: [
+      {title: 'Male-Oriented SPA', value: 'MSPA'},
+      {title: 'Female-Oriented  SPA', value: 'FSPA'},
+    ]
+  },
+  {
+    title: 'Hospitals-Residents problem',
+    value: 'HR',
+    algs: [
+      {title: 'Resident-oriented Gale/Shapley algorithm', value: 'RGS'},
+      {title: 'Hospitals-oriented Gale/Shapley algorithm', value: 'HGS'},
+    ]
+  }
+];
+
+
+export default defineComponent({
   data: () => ({
-    firstName: '',
+    matchingName: '',
+    problemSelected: {} as Problem,
+    algSelected: {} as Item | null,
+    problemData: problem_data,
+    inputFile: null,
     rules: [
       (value: string) => {
         if (value) return true
@@ -11,24 +81,25 @@ export default {
       },
     ],
   }),
-}
+  mounted() {
+    // get supported problems algs;
+    this.problemSelected = this.problemData[0];
+  },
+  methods: {
+    problemOnChange() {
+      this.algSelected = this.problemSelected.algs[1];
+    },
+    onSubmit() {
+
+    }
+  },
+  computed: {
+    algsList() {
+      return this.problemSelected.algs;
+    },
+  }
+})
 
 </script>
-
-<template>
-  <div>
-    <v-sheet width="300" class="mx-auto">
-      <v-form @submit.prevent>
-        <v-text-field
-            v-model="firstName"
-            :rules="rules"
-            label="First name"
-        ></v-text-field>
-        <v-btn type="submit" block class="mt-2">Submit</v-btn>
-      </v-form>
-    </v-sheet>
-  </div>
-</template>
-
 <style scoped>
 </style>
--- MathingPlatformUI/MatchingPlatform/src/main.ts
+++ MathingPlatformUI/MatchingPlatform/src/main.ts
@@ -7,8 +7,19 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 const vuetify = createVuetify({
+    theme: {
+        defaultTheme: 'dark'
+    },
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        }
+    },
     components,
     directives,
 })
